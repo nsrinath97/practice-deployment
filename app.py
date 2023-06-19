082d58ebd1f03eef55830933d71a1498401a6708
Srinath Nanduri <67294707+nsrinath97@users.noreply.github.com>
2023-06-19 20:41:20 +0000

    Added API route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 tokenizer = pickle.load(open("models/cv.pkl", "rb"))
@@ -12,12 +12,20 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    if request.method == "POST":
-        email_text = request.form.get("email-content")
+    email_text = request.form.get("email-content")
     tokenized_email = tokenizer.transform([email_text])
     prediction = model.predict(tokenized_email)
     prediction = 1 if prediction == 1 else -1
     return render_template("index.html", prediction=prediction, email_text=email_text)
+
+@app.route("/api/predict", methods=["POST"])
+def api_predict():
+    data = request.get_json(force=True)
+    email_text = data["content"]
+    tokenized_email = tokenizer.transform([email_text])
+    prediction = model.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({'prediction': prediction, 'email_text': email_text})
 
 if __name__ =="__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
